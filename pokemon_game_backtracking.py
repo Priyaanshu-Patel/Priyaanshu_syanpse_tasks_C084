5392bd5e84330b8c9ed5607bb3d2e9e2dd980ab2
Priyaanshu-Patel <101932415+Priyaanshu-Patel@users.noreply.github.com>
2025-08-29 19:23:29 +0530

    Update pokemon_game_backtracking.py

--- pokemon_game_backtracking.py
+++ pokemon_game_backtracking.py
@@ -12,19 +12,6 @@ Pokedex = {
 }
 
 
-Pokedex = {
-    "Pikachu": ("Electric",),
-    "Charizard": ("Fire", "Flying"),
-    "Lapras": ("Water", "Ice"),
-    "Machamp": ("Fighting",),
-    "Mewtwo": ("Psychic", "Fighting"),
-    "Hoopa": ("Psychic", "Ghost", "Dark"),
-    "Lugia": ("Psychic", "Flying", "Water"),
-    "Squirtle": ("Water",),
-    "Gengar": ("Ghost", "Poison"),
-    "Onix": ("Rock", "Ground")
-}
-
 
 best_team = []
 best_types = set()
